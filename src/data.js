359b807f7b9bc68ac24a886706bf5c550595856b
Ddnt2409 <Contato.dudunite@gmail.com>
2025-06-24 20:10:19 -0300

    Create data.js

---
+++ src/data.js
@@ -0,0 +1,18 @@
+export const cidades = [
+  "Gravatá",
+  "Recife",
+  "Caruaru",
+  "Olinda"
+];
+
+export const escolas = [
+  { id: 1, nome: "Escola Municipal A", cidade: "Gravatá" },
+  { id: 2, nome: "Escola Estadual B", cidade: "Recife" },
+  { id: 3, nome: "Escola Particular C", cidade: "Caruaru" },
+];
+
+export const produtosPorEscola = {
+  1: ["Brownie 7x7", "Pocket 5x5"],
+  2: ["Brownie 6x6", "Escondidinho"],
+  3: ["Brownie 7x7", "Escondidinho", "Pocket 6x6"]
+};
