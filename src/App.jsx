6ecec4bf4bc0eeb2fcb86b58b2b2a0d5ea8c7078
Ddnt2409 <Contato.dudunite@gmail.com>
2025-07-22 19:36:37 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -616,7 +616,7 @@ return (
       </div> {/* FIM dos botões */}
     </div> {/* FIM do container interno */}
   </div> 
-))} {/* FIM condicional RT03 */}
+))}} {/* FIM condicional RT03 */}
 {/* === FIM RT03 === */}
 </> // FIM do fragmento
 ); // FIM do return
