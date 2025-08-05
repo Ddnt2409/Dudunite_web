d98d22388113cc113c532a52f129633645778195
Ddnt2409 <Contato.dudunite@gmail.com>
2025-08-05 18:04:56 -0300

    Update HomePCP.jsx

--- src/pages/HomePCP.jsx
+++ src/pages/HomePCP.jsx
@@ -1,38 +1,52 @@
-import React from 'react';
-import './HomePCP.css';
+import React from 'react'
+import './HomePCP.css'
 
-const HomePCP = ({ setTela }) => {
+export default function HomePCP({ setTela }) {
   return (
     <div className="homepcp-container">
-      {/* === INÍCIO RT00 – Tela Home PCP === */}
-
-      <div className="homepcp-header">
-        <img src="/LogomarcaDDnt2025Vazado.png" alt="Logo Dudunitê" className="logo-pcp" />
+      {/* HEADER */}
+      <header className="homepcp-header">
+        <img
+          src="/LogomarcaDDnt2025Vazado.png"
+          alt="Logo Dudunitê"
+          className="logo-pcp"
+        />
         <h1 className="homepcp-titulo">PCP – Planejamento de Produção</h1>
-      </div>
+      </header>
 
+      {/* BOTÕES */}
       <div className="botoes-pcp">
-        <button className="botao-principal botao-ativo" onClick={() => setTela("LanPed")}>
-          📝<br />Lançar Pedido (Teste)
+        <button
+          className="botao-principal botao-ativo"
+          onClick={() => setTela('LanPed')}
+        >
+          📝<br />
+          Lançar Pedido
         </button>
 
-        {/* Temporariamente desativado até criar o componente AlimSab */}
-        {/* <button className="botao-principal botao-ativo" onClick={() => setTela("AlimSab")}>
-          🍫<br />Alimentar Sabores
-        </button> */}
+        <button className="botao-principal botao-inativo">
+          🍫<br />
+          Alimentar Sabores
+        </button>
       </div>
 
-      <button className="botao-voltar" onClick={() => setTela("HomeERP")}>
+      {/* VOLTAR */}
+      <button
+        className="botao-voltar"
+        onClick={() => setTela('HomeERP')}
+      >
         🔙 Voltar ao ERP
       </button>
 
-      <div className="lista-escolas">
-        Cruz • Pinheiros • Dourado • BMQ • CFC • Madre de Deus • Saber Viver • Interativo • Exato Sede • Exato Anexo • Society Show • Russas • Kaduh • Degusty • Bora Gastar • Salesianas • Céu Azul • Pequeno Príncipe • Tio Valter • Vera Cruz
-      </div>
-
-      {/* === FIM RT00 === */}
+      {/* MARQUEE RODAPÉ */}
+      <footer className="lista-escolas">
+        <marquee behavior="scroll" direction="left">
+          • Cruz • Pinheiros • Dourado • BMQ • CFC • Madre de Deus • Saber Viver
+          • Interativo • Exato Sede • Exato Anexo • Society Show • Russas •
+          Kaduh • Degusty • Bora Gastar • Salesianas • Céu Azul • Pequeno
+          Príncipe • Tio Valter • Vera Cruz
+        </marquee>
+      </footer>
     </div>
-  );
-};
-
-export default HomePCP;
+  )
+}
