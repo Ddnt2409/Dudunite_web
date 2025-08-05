eb6f56bc7684403ebfea268e283558c8af532bfc
Ddnt2409 <Contato.dudunite@gmail.com>
2025-08-04 22:00:31 -0300

    Update LanPed.jsx

--- src/pages/LanPed.jsx
+++ src/pages/LanPed.jsx
@@ -5,33 +5,16 @@ function LanPed({ setTela }) {
     <div
       style={{
         backgroundColor: '#fff5ec',
-        minHeight: '100dvh',
+        height: '100dvh',
         display: 'flex',
-        flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'center',
-        color: '#8c3b1b',
         fontSize: '2rem',
         fontWeight: 'bold',
+        color: '#8c3b1b',
       }}
     >
-      <p>👍 LanPed OK!</p>
-
-      <button
-        style={{
-          marginTop: '2rem',
-          backgroundColor: '#8c3b1b',
-          color: 'white',
-          padding: '0.8rem 2rem',
-          border: 'none',
-          borderRadius: '8px',
-          cursor: 'pointer',
-          fontSize: '1.2rem',
-        }}
-        onClick={() => setTela('HomePCP')}
-      >
-        Voltar
-      </button>
+      👍 LanPed OK!
     </div>
   );
 }
