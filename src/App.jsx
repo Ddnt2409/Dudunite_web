c25bc8458eb6cdd88dfafb1b06d2eb0fb846fc99
Ddnt2409 <Contato.dudunite@gmail.com>
2025-08-04 21:10:36 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-// App.jsx
 import React, { useState } from 'react';
 import HomeERP from './pages/HomeERP';
 import HomePCP from './pages/HomePCP';
 import LanPed from './pages/LanPed';
 
 function App() {
-  const [telaAtual, setTelaAtual] = useState('HomePCP');
+  const [telaAtual, setTelaAtual] = useState('HomeERP'); // 👈 VOLTA A SER A TELA PRINCIPAL
 
   if (telaAtual === 'HomeERP') return <HomeERP setTela={setTelaAtual} />;
   if (telaAtual === 'HomePCP') return <HomePCP setTela={setTelaAtual} />;
