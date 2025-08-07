1183b50831332a5544b65223350961b14c1bb57d
Ddnt2409 <Contato.dudunite@gmail.com>
2025-08-06 22:03:07 -0300

    Update HomePCP.jsx

--- src/pages/HomePCP.jsx
+++ src/pages/HomePCP.jsx
@@ -4,54 +4,53 @@ import './HomePCP.css';
 export default function HomePCP({ setTela }) {
   return (
     <div className="homepcp-container">
-      {/* HEADER */}
-      <header className="homepcp-header">
+      {/* === HEADER === */}
+      <div className="homepcp-header">
         <img
           src="/LogomarcaDDnt2025Vazado.png"
           alt="Logo Dudunitê"
           className="logo-pcp"
         />
         <h1 className="homepcp-titulo">PCP – Planejamento de Produção</h1>
-      </header>
+      </div>
 
-      {/* BOTÕES PRINCIPAIS */}
-      <main className="botoes-pcp">
-        {/* Lançar Pedido */}
-        <button
-          className="botao-principal botao-ativo"
-          onClick={() => setTela('LanPed')}
-        >
-          📝<br />
-          Lançar Pedido
-        </button>
+      {/* === BOTÕES PRINCIPAIS === */}
+      <div className="botoes-pcp">
+        <div className="botao-wrapper">
+          <button
+            className="botao-principal botao-ativo"
+            onClick={() => setTela('LanPed')}
+          >
+            📝<br />
+            Lançar Pedido
+          </button>
+        </div>
 
-        {/* Alimentar Sabores */}
-        <button
-          className="botao-principal botao-inativo"
-          onClick={() => alert('Em construção')}
-        >
-          🍫<br />
-          Alimentar Sabores
-        </button>
-      </main>
+        <div className="botao-wrapper">
+          <button
+            className="botao-principal botao-inativo"
+            onClick={() => alert('Em construção')}
+          >
+            🍫<br />
+            Alimentar Sabores
+          </button>
+        </div>
+      </div>
 
-      {/* VOLTAR */}
-      <button
-        className="botao-voltar"
-        onClick={() => setTela('HomeERP')}
-      >
+      {/* === VOLTAR === */}
+      <button className="botao-voltar" onClick={() => setTela('HomeERP')}>
         🔙 Voltar ao ERP
       </button>
 
-      {/* RODAPÉ */}
-{/* RODAPÉ ANIMADO */}
-<div className="lista-escolas">
-  <span className="marquee-content">
-    • Cruz • Pinheiros • Dourado • BMQ • CFC • Madre de Deus • Saber Viver •
-    Interativo • Exato Sede • Exato Anexo • Society Show • Russas • Kaduh •
-    Degusty • Bora Gastar • Salesianas • Céu Azul • Pequeno Príncipe •
-    Tio Valter • Vera Cruz
-  </span>
-</div>
+      {/* === RODAPÉ ANIMADO === */}
+      <div className="lista-escolas">
+        <span className="marquee-content">
+          • Cruz • Pinheiros • Dourado • BMQ • CFC • Madre de Deus • Saber Viver •
+          Interativo • Exato Sede • Exato Anexo • Society Show • Russas • Kaduh •
+          Degusty • Bora Gastar • Salesianas • Céu Azul • Pequeno Príncipe •
+          Tio Valter • Vera Cruz
+        </span>
+      </div>
+    </div>
   );
 }
