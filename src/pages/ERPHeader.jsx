14c41dfed925a287059e10aaca8b39e54d2a8de4
Ddnt2409 <Contato.dudunite@gmail.com>
2025-08-11 08:01:58 -0300

    Update ERPHeader.jsx

--- src/pages/ERPHeader.jsx
+++ src/pages/ERPHeader.jsx
@@ -6,11 +6,7 @@ export default function ERPHeader({ title }) {
         <div className="erp-header__logo">
           <img src="/LogomarcaDDnt2025Vazado.png" alt="Dudunitê" />
         </div>
-
         <h1 className="erp-header__title">{title}</h1>
-
-        {/* espaçador para manter o título 100% central */}
-        <div className="erp-header__spacer" aria-hidden />
       </div>
     </header>
   );
